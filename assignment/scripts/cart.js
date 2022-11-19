5f6790dd41447a1fa5ed532d62210fbd7fe28b40
Farah Mire <farah_mire@yahoo.com>
2022-11-19 11:18:17 -0600

    changes

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,45 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+
+//1. 
+let basket= [ 1, 4, 5];
+
+function addItem(Item) { 
+basket.push(Item);
+return true;
+}
+
+
+console.log(addItem('cherry'));
+
+console.log(basket);
+
+
+
+//2. 
+
+
+
+function listItems(basket){
+    for (i=0 ; i<basket.length ; i++){ 
+    console.log(basket[i])
+    } 
+return basket;
+}
+
+console.log(basket);
+
+
+//3. 
+
+function empty(){
+    return basket = [];
+}
+console.log(empty());
+
+
+
+
+
